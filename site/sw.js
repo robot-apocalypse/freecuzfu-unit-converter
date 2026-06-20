8caece109f2592e9872f094d30db53669e33dc45
Ian Buffington <ian.buffington@gmail.com>
2026-06-20 14:35:23 +0000

    Add GitHub link and bump service worker cache

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'unit-converter-v1';
+const CACHE = 'unit-converter-v2';
 const ASSETS = ['/', '/style.css', '/app.js', '/manifest.json', '/icons/icon-192.svg', '/icons/icon-512.svg'];
 
 self.addEventListener('install', (e) => {
